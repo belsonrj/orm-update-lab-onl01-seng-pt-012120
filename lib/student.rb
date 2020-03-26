e09212dff2865be61342d582a8068436172ed520
Rick Belson <belsonrj@aol.com>
2020-03-26 01:00:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :id, :name, :grade
   
-  def initialize()
+  def initialize(id=nil, name, grade)
+    
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
